caf946027963fc91d33dbad3b6f5a61974368eed
yatinagg <yatinagg12@gmail.com>
2021-12-25 11:24:49 +0530

    Logout feature added

--- app/src/main/java/com/example/todo/MainActivity.kt
+++ app/src/main/java/com/example/todo/MainActivity.kt
@@ -4,8 +4,11 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
+import androidx.recyclerview.widget.RecyclerView
 import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.auth.api.signin.GoogleSignInClient
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
@@ -14,6 +17,9 @@ import com.google.android.gms.common.api.ApiException
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.auth.GoogleAuthProvider
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 
 const val REQUEST_ID = 101
 const val TAG = "MainActivity"
@@ -22,6 +28,10 @@ class MainActivity : AppCompatActivity() {
     lateinit var gso : GoogleSignInOptions
     lateinit var mSignClient: GoogleSignInClient
     lateinit var firebaseAuth: FirebaseAuth
+    lateinit var db: FirebaseFirestore
+    lateinit var tvDisplayName: TextView
+    lateinit var todoRecyclerView: RecyclerView
+    lateinit var logoutButton: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -29,15 +39,24 @@ class MainActivity : AppCompatActivity() {
         findViewById<SignInButton>(R.id.sign_in_button).setOnClickListener {
             signIn()
         }
+        tvDisplayName = findViewById(R.id.tv_display_name)
+        todoRecyclerView = findViewById(R.id.todo_recycler_view)
+        logoutButton = findViewById(R.id.log_out_button)
 
         gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
             .requestIdToken(getString(R.string.token))
             .requestEmail()
             .build()
 
+        logoutButton.setOnClickListener {
+            logoutFromGoogle()
+        }
+
 
         firebaseAuth = FirebaseAuth.getInstance()
         mSignClient = GoogleSignIn.getClient(this, gso)
+
+        db = Firebase.firestore
     }
 
     private fun signIn() {
@@ -67,6 +86,13 @@ class MainActivity : AppCompatActivity() {
             .addOnCompleteListener(this) { task ->
                 if (task.isSuccessful) {
                     Log.d(TAG, "sign in success")
+                    Toast.makeText(this,"Sign in successful",Toast.LENGTH_SHORT).show()
+                    findViewById<SignInButton>(R.id.sign_in_button).visibility = View.GONE
+                    findViewById<TextView>(R.id.tv_login).visibility = View.GONE
+                    tvDisplayName.visibility = View.VISIBLE
+                    todoRecyclerView.visibility = View.VISIBLE
+                    logoutButton.visibility = View.VISIBLE
+
                     val user = firebaseAuth.currentUser
                     updateUI(user)
                 } else {
@@ -76,6 +102,14 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun updateUI(user: FirebaseUser?) {
+        println("${firebaseAuth.currentUser}wow")
+        tvDisplayName.text = user?.displayName.toString()
         Log.d(TAG, user?.displayName.toString())
+    }
+
+    private fun logoutFromGoogle(){
+        firebaseAuth.signOut()
+        Toast.makeText(this,"LogOut successful",Toast.LENGTH_SHORT).show()
+        println("${firebaseAuth.currentUser}wow")
     }
 }
---
+++ app/src/main/java/com/example/todo/User.kt
@@ -0,0 +1,3 @@
+package com.example.todo
+
+data class User(val id: String, val name: String, val todos: List<String>)
